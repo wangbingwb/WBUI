f6e48fcc9b4a8a9b5200d47906447a40958181b3
wangbing <wangbingit@outlook.com>
2018-09-05 09:11:56 +0800

    优化

--- app/src/main/java/xyz/wbsite/wbsiteui/witget/WBUIGridView.java
+++ app/src/main/java/xyz/wbsite/wbsiteui/witget/WBUIGridView.java
@@ -3,10 +3,14 @@ package xyz.wbsite.wbsiteui.witget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.GridView;
+
 import xyz.wbsite.wbsiteui.R;
 
 public class WBUIGridView extends GridView {
@@ -26,20 +30,21 @@ public class WBUIGridView extends GridView {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         int cols = getNumColumns(); //获取列数
-        int rows = getChildCount() / cols + getChildCount() % cols > 0 ? 1 : 0;  //获取行数
 
         Paint localPaint; //设置画笔
         localPaint = new Paint();
-        localPaint.setStyle(Paint.Style.STROKE); //画笔实心
-        localPaint.setColor(getContext().getResources().getColor(R.color.colorDividerLine5));//画笔颜色
+        localPaint.setStyle(Paint.Style.FILL); //画笔实心
 
         for (int i = 0; i < getChildCount(); i++) {
             View childAt = getChildAt(i);
-
-            canvas.drawLine(childAt.getLeft(), childAt.getBottom(), childAt.getRight(), childAt.getBottom(), localPaint);
+            localPaint.setShader(new LinearGradient(childAt.getLeft(), childAt.getBottom(), childAt.getRight(), childAt.getBottom(), new int[]{
+                    Color.TRANSPARENT, getResources().getColor(R.color.colorDividerLine6), Color.TRANSPARENT}, null, Shader.TileMode.REPEAT));
+            canvas.drawLine(childAt.getLeft(), childAt.getBottom() - 1, childAt.getRight(), childAt.getBottom() - 1, localPaint);
 
             if ((i + 1) % cols != 0) {
-                canvas.drawLine(childAt.getRight(), childAt.getTop(), childAt.getRight(), childAt.getBottom(), localPaint);
+                localPaint.setShader(new LinearGradient(childAt.getRight(), childAt.getTop(), childAt.getRight(), childAt.getBottom(), new int[]{
+                        Color.TRANSPARENT, getResources().getColor(R.color.colorDividerLine6), Color.TRANSPARENT}, null, Shader.TileMode.MIRROR));
+                canvas.drawLine(childAt.getRight() - 1, childAt.getTop(), childAt.getRight() - 1, childAt.getBottom(), localPaint);
             }
         }
     }
