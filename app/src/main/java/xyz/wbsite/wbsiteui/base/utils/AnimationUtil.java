41c901f0831b1509f9d6cec2369be333e7d400a8
wangbing <wangbingit@outlook.com>
2018-10-18 14:34:58 +0800

    1、动画工具

--- app/src/main/java/xyz/wbsite/wbsiteui/base/utils/AnimationUtil.java
+++ app/src/main/java/xyz/wbsite/wbsiteui/base/utils/AnimationUtil.java
@@ -8,120 +8,127 @@ import android.view.animation.TranslateAnimation;
 
 public class AnimationUtil {
 
-    private static boolean ismHiddenActionstart = false;
-
-    public static Animation getShakeAnimation(int counts) {
+    public static void shake(View view, int counts, Animation.AnimationListener listener) {
         Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
         translateAnimation.setInterpolator(new CycleInterpolator(counts));
         translateAnimation.setDuration(500);
-        return translateAnimation;
+        translateAnimation.setAnimationListener(listener);
+        view.startAnimation(translateAnimation);
     }
 
-    /**
-     * 从控件所在位置移动到控件的底部
-     *
-     * @param v
-     * @param Duration 动画时间
-     */
-    public static void moveToViewBottom(final View v, long Duration) {
-        if (v.getVisibility() != View.VISIBLE)
-            return;
-        if (ismHiddenActionstart)
-            return;
-        TranslateAnimation mHiddenAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+    public static void shake(View view, int counts) {
+        shake(view, counts, null);
+    }
+
+    public static void toBottom(final View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
                 Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
                 0.0f, Animation.RELATIVE_TO_SELF, 1.0f);
-        mHiddenAction.setDuration(Duration);
-        v.clearAnimation();
-        v.setAnimation(mHiddenAction);
-        mHiddenAction.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                ismHiddenActionstart = true;
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                v.setVisibility(View.GONE);
-                ismHiddenActionstart = false;
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
     }
 
-    /**
-     * 从控件的底部移动到控件所在位置
-     *
-     * @param v
-     * @param Duration 动画时间
-     */
-    public static void bottomMoveToViewLocation(View v, long Duration) {
-        if (v.getVisibility() == View.VISIBLE)
-            return;
-        v.setVisibility(View.VISIBLE);
-        TranslateAnimation mShowAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
-                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
-                1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
-        mShowAction.setDuration(Duration);
-        v.clearAnimation();
-        v.setAnimation(mShowAction);
+    public static void toBottom(View view, long duration) {
+        toBottom(view, duration, null);
     }
 
-    /**
-     * 从控件所在位置移动到控件的顶部
-     *
-     * @param v
-     * @param Duration 动画时间
-     */
-    public static void moveToViewTop(final View v, long Duration) {
-        if (v.getVisibility() != View.VISIBLE)
-            return;
-        if (ismHiddenActionstart)
-            return;
-        TranslateAnimation mHiddenAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+    public static void toTop(View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
                 Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
                 0.0f, Animation.RELATIVE_TO_SELF, -1.0f);
-        mHiddenAction.setDuration(Duration);
-        v.clearAnimation();
-        v.setAnimation(mHiddenAction);
-        mHiddenAction.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                ismHiddenActionstart = true;
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                v.setVisibility(View.GONE);
-                ismHiddenActionstart = false;
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
     }
 
-    /**
-     * 从控件的顶部移动到控件所在位置
-     *
-     * @param v
-     * @param Duration 动画时间
-     */
-    public static void topMoveToViewLocation(View v, long Duration) {
-        if (v.getVisibility() == View.VISIBLE)
-            return;
-        v.setVisibility(View.VISIBLE);
-        TranslateAnimation mShowAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+    public static void toTop(View view, long duration) {
+        toTop(view, duration, null);
+    }
+
+    public static void toLeft(View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, -1.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
+    }
+
+    public static void toLeft(View view, long duration) {
+        toLeft(view, duration, null);
+    }
+
+    public static void toRight(final View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
+    }
+
+    public static void toRight(final View view, long duration) {
+        toRight(view, duration, null);
+    }
+
+    public static void fromBottom(View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
+    }
+
+    public static void fromBottom(View view, long duration) {
+        fromBottom(view, duration, null);
+    }
+
+    public static void fromTop(View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
                 Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
                 -1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
-        mShowAction.setDuration(Duration);
-        v.clearAnimation();
-        v.setAnimation(mShowAction);
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
+    }
+
+    public static void fromTop(View view, long duration) {
+        fromTop(view, duration, null);
+    }
+
+    public static void fromLeft(View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, -1.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
+    }
+
+    public static void fromLeft(View view, long duration) {
+        fromLeft(view, duration, null);
+    }
+
+    public static void fromRight(View view, long duration, Animation.AnimationListener listener) {
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        animation.setDuration(duration);
+        animation.setAnimationListener(listener);
+        view.clearAnimation();
+        view.startAnimation(animation);
+    }
+
+    public static void fromRight(View view, long duration) {
+        fromRight(view, duration, null);
     }
 }
