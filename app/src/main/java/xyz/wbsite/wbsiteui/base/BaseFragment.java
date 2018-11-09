fc4a74f8bab2d1a78d089cd8bbb487d5c8011eb4
wangbing <wangbingit@outlook.com>
2018-11-09 10:03:36 +0800

    1、优化

--- app/src/main/java/xyz/wbsite/wbsiteui/base/BaseFragment.java
+++ app/src/main/java/xyz/wbsite/wbsiteui/base/BaseFragment.java
@@ -1,11 +1,37 @@
 package xyz.wbsite.wbsiteui.base;
 
+import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public abstract class BaseFragment extends Fragment {
-
+    private Unbinder unbinder;
     protected Handler handler = new Handler();
 
     protected abstract void initView();
+
+    protected abstract int getFragmnetLayout();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View inflate = LayoutInflater.from(getActivity()).inflate(getFragmnetLayout(), null);
+        unbinder = ButterKnife.bind(this, inflate);
+        initView();
+        return inflate;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
 }
