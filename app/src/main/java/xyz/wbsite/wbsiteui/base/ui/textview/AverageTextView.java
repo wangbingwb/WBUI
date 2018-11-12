ddeb87162f04476573c8b0981ff3bf1704855381
wangbing <wangbingit@outlook.com>
2018-11-12 15:01:40 +0800

    1、AverageTextView

---
+++ app/src/main/java/xyz/wbsite/wbsiteui/base/ui/textview/AverageTextView.java
@@ -0,0 +1,60 @@
+package xyz.wbsite.wbsiteui.base.ui.textview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+
+public class AverageTextView extends android.support.v7.widget.AppCompatTextView {
+
+    public AverageTextView(Context context) {
+        super(context);
+    }
+
+    public AverageTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        TextPaint paint = getPaint();
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setColor(getCurrentTextColor());
+
+        String mText = getText().toString();
+        int width = getWidth() - getPaddingLeft() - getPaddingRight();
+        int height = getHeight() - getPaddingTop() - getPaddingBottom();
+        int x_offset = getPaddingLeft();
+        int y_offset = getPaddingTop();
+
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        float offset = (Math.abs(fontMetrics.top) - fontMetrics.bottom) / 2;
+
+        if (mText.length() >= 2) {
+            String firstF = String.valueOf(mText.charAt(0));
+            String lastF = String.valueOf(mText.charAt(mText.length() - 1));
+
+            float firstF_W = paint.measureText(firstF);
+            float lastF_W = paint.measureText(lastF);
+
+            {// 绘制首位字符
+                canvas.drawText(firstF, firstF_W / 2 + x_offset, height / 2 + offset + y_offset, paint);
+                canvas.drawText(lastF, width - lastF_W / 2 + x_offset, height / 2 + offset + y_offset, paint);
+            }
+
+            // 如果大于2个才均分
+            if (mText.length() > 2) {
+                float remainingWidth = width - firstF_W - lastF_W;//剩余宽度
+                float averageW = remainingWidth / (mText.length() - 2);
+
+                for (int i = 1; i < mText.length() - 1; i++) {//循环绘文字
+                    String f = String.valueOf(mText.charAt(i));
+                    canvas.drawText(f, firstF_W + (i - 1) * averageW + averageW / 2 + x_offset, height / 2 + offset + y_offset, paint);
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/xyz/wbsite/wbsiteui/fragment/functions/AverageTextViewFragment.java
@@ -0,0 +1,28 @@
+package xyz.wbsite.wbsiteui.fragment.functions;
+
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+
+import butterknife.BindView;
+import xyz.wbsite.wbsiteui.R;
+import xyz.wbsite.wbsiteui.base.BaseSPAFragment;
+
+public class AverageTextViewFragment extends BaseSPAFragment {
+
+    @BindView(R.id.topbar)
+    QMUITopBarLayout topbar;
+
+    @Override
+    protected int getFragmnetLayout() {
+        return R.layout.fragment_average_textview;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected boolean canDragBack() {
+        return false;
+    }
+}
