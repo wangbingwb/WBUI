f72b238d20233b6572debee6157a598d6331a09e
wangbing <wangbingit@outlook.com>
2019-02-13 11:19:12 +0800

    1、文件名称

--- app/src/main/java/xyz/wbsite/wbsiteui/base/ui/dialog/WBUIDialog.java
+++ app/src/main/java/xyz/wbsite/wbsiteui/base/ui/dialog/WBUIDialog.java
@@ -12,11 +12,11 @@ public class WBUIDialog extends Dialog {
 
     public void show() {
         super.show();
-        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) mContentView
-                .getLayoutParams();
-        layoutParams.width = display.getWidth();
-        layoutParams.height = display.getHeight();
-        mContentView.setLayoutParams(layoutParams);
+//        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) mContentView
+//                .getLayoutParams();
+//        layoutParams.width = display.getWidth();
+//        layoutParams.height = display.getHeight();
+//        mContentView.setLayoutParams(layoutParams);
 
     }
 }
